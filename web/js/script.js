2b4a944a66f42a98ee994004d1312b4f9f501b44
Ajay ps <ajaypalamkunnel45@gmail.com>
2023-12-18 10:54:49 +0530

    Testimonial content added

--- web/js/script.js
+++ web/js/script.js
@@ -60,3 +60,17 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+
+
+$(document).ready(function(){
+  $("#testimonial-slider").owlCarousel({
+      items:2,
+      itemsDesktop:[1000,2],
+      itemsDesktopSmall:[980,1],
+      itemsTablet:[768,1],
+      pagination:true,
+      navigation:true,
+      navigationText:["<",">"],
+      autoPlay:true
+  });
+});
